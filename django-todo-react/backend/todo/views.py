6ca662a0f99c6175f1ee3caea46bd7f14237ec0a
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-10-04 07:51:42 +0545

    views

--- django-todo-react/backend/todo/views.py
+++ django-todo-react/backend/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializers
+from .models import Todo
 
-# Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializers
+    queryset = Todo.objects.all()
+    
