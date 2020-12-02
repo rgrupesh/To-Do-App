436d6132b817544c2973ce6d38723211e1faff01
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-12-02 18:29:42 +0545

    theme change button

--- django-todo-react/frontend/src/components/TodoForm.jsx
+++ django-todo-react/frontend/src/components/TodoForm.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Container, TextField, Button, FormControl } from "@material-ui/core";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const TodoForm = () => {
   return (
     <div>
+      <FormControlLabel
+        control={<Switch color="primary" />}
+        label="Change theme"
+      />
       <Container maxWidth="sm">
         <form>
           <FormControl fullWidth={true}>
